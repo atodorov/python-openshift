d24d9d938202fe0eea751b51d9fa3481df444024
Alexander Todorov <atodorov@otb.bg>
2011-11-30 23:17:48 +0200

    add a setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#   Copyright (c) 2011, Alexander Todorov <atodorov()otb.bg>
+
+from distutils.core import setup
+
+setup(name='python-openshift',
+      version='0.1',
+      description='Python interface to Red Hat's OpenShift cloud platform',
+      author='Alexander Todorov',
+      author_email='atodorov@nospam.otb.bg',
+      url='https://github.com/atodorov/python-openshift',
+      packages = ['openshift'],
+)
